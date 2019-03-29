02be1d323bb4341165a6435093225162f62acc59
Dragon Black <db87987@gmail.com>
2019-03-29 15:23:41 +0300

    extend map attributes

--- lib/map.rb
+++ lib/map.rb
@@ -1,18 +1,19 @@
 class Map
-  attr_reader :file
+  attr_reader :path, :title, :description, :difficulty, :size, :players, :humans, :computers
 
   def initialize(path)
+    @path = path
     @file = File.read(path);
     # magic_byte = file.unpack("N").first
     # raise "Incorrect map file" unless magic_byte == 1543503872
   end
 
   def title
-    file.unpack("x58Z*").first
+    @file.unpack("x58Z*").first
   end
 
   def description
-    file.unpack("x118Z*").first
+    @file.unpack("x118Z*").first
   end
 
   def difficulties
@@ -25,21 +26,80 @@ class Map
   end
 
   def difficulty
-    level = file.unpack("x4S").first
+    level = @file.unpack("x4S").first
     difficulties[level]
   end
 
+  def width
+    @file.unpack("x6C").first
+  end
+
+  # def height
+  #   @file.unpack("x7C").first
+  # end
+
+  def sizes
+    {
+      36 => "S",
+      72 => "M",
+      108 => "L",
+      144 => "XL",
+    }
+  end
+
+  # def kingdom_colors
+  #   colors.each_with_object([]).with_index do |(enabled, memo), index|
+  #     memo << color_by_index[index] if enabled == 1
+  #   end
+  # end
+
+  # def color_by_index
+  #   {
+  #     0 => "Blue",
+  #     1 => "Green",
+  #     2 => "Red",
+  #     3 => "Yellow",
+  #     4 => "Orange",
+  #     5 => "Purple",
+  #   }
+  # end
+
+  def size
+    sizes[width]
+  end
+
+  # def colors
+  #   @file.unpack("x8C6")
+  # end
+
+  def players
+    @file.unpack("x8C6").reject { |n| n == 0 }.count
+  end
+
+  def humans
+    @file.unpack("x14C6").reject { |n| n == 0 }.count
+  end
+
+  def computers
+    @file.unpack("x20C6").reject { |n| n == 0 }.count
+  end
+
   def attributes
-    [title, description, difficulty]
-    # {
-    #   title: title,
-    #   description: description,
-    #   difficulty: difficulty,
-    # }
+    # [title, description, difficulty]
+    {
+      title: title,
+      description: description,
+      difficulty: difficulty,
+      size: size,
+      players: players,
+      humans: humans,
+      computers: computers,
+      file: path
+    }
   end
 end
 
-# // 0x4       4     Difficulty          2 bytes
+
 # // 0x6       6     Width               1 byte
 # // 0x7       7     Height              1 byte
 # // 0x8       8     KingdomColors       6 bytes
